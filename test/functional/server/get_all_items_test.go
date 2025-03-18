cfc4411fc9378b9fbdb9d50e8c6508ce585006ac
Vladislav Duplin <vaduplin@avito.ru>
2025-03-18 17:16:25 +0300

    server: test get all items limit offset

--- test/functional/server/get_all_items_test.go
+++ test/functional/server/get_all_items_test.go
@@ -37,6 +37,39 @@ func (s *TestSuite) TestGetAllItems() {
 		s.Assert().Len(getAllItemsOut.GetItems(), 4)
 	})
 
+	s.Run("success limit offset", func() {
+		ctx := context.Background()
+		loginOut, err := s.client.Login(ctx, &rpc.LoginIn{
+			Email:    "test@test.ru",
+			Password: "12345678",
+		})
+		s.Require().NoError(err)
+
+		md, ok := metadata.FromOutgoingContext(ctx)
+		if !ok {
+			md = metadata.New(nil)
+		}
+		md.Set("authorization", fmt.Sprintf("%s %s", "Bearer", loginOut.GetAccessToken()))
+
+		getAllItemsOut1, err := s.client.GetAllItems(metadata.NewOutgoingContext(ctx, md), &rpc.GetAllItemsIn{
+			Offset: 0,
+			Limit:  2,
+		})
+		s.Require().NoError(err)
+		s.Assert().Len(getAllItemsOut1.GetItems(), 2)
+		s.Assert().Equal(getAllItemsOut1.GetItems()[0].GetGuid(), "127e1a2d-1943-4fb1-ba60-7dc4fc820ed4")
+		s.Assert().Equal(getAllItemsOut1.GetItems()[1].GetGuid(), "60308368-7729-4d2d-a510-67926f5a159b")
+
+		getAllItemsOut2, err := s.client.GetAllItems(metadata.NewOutgoingContext(ctx, md), &rpc.GetAllItemsIn{
+			Offset: 2,
+			Limit:  2,
+		})
+		s.Require().NoError(err)
+		s.Assert().Len(getAllItemsOut2.GetItems(), 2)
+		s.Assert().Equal(getAllItemsOut2.GetItems()[0].GetGuid(), "6e7fc4fa-31aa-4d75-8b6e-0479122e0147")
+		s.Assert().Equal(getAllItemsOut2.GetItems()[1].GetGuid(), "b2bd09eb-2c84-4149-b2b8-29040472264a")
+	})
+
 	s.Run("permission denied", func() {
 		ctx := context.Background()
 		out, err := s.client.GetAllItems(ctx, &rpc.GetAllItemsIn{})
